a44a4f9ce67d5b7eb6324d6d2662ff6e2fac6c3b
Nattapatque <nattapatque@gmail.com>
2024-03-26 12:11:36 +0700

    debug register

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,6 +15,7 @@ exports.register = async (req,res,next)=>{
         sendTokenResponse(user,200,res)
     }catch(err){
         res.status(400).json({success:false});
+        console.log("-----------");
         console.log(err.stack);
     }
 }
